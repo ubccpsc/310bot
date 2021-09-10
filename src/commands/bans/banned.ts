fbb98498a577f2e8a543272dec77aefa220e79a0
Bryce Wilson <bryce@brycemw.ca>
2021-09-09 22:33:59 -0700

    Replace the middle vowel with a *

--- src/commands/bans/banned.ts
+++ src/commands/bans/banned.ts
@@ -7,7 +7,19 @@ const banned: Command = {
     description: "Lists the banned word (if any)",
     usage: "banned",
     procedure: async (client: Client, message: Message) => {
-        const bannedWord = await getBannedWord();
+        let bannedWord = await getBannedWord();
+        const middle = (bannedWord.length / 2.0) + 0.5; // Adding 0.5 is the equivalent of ensuring it always rounds up
+        // Find the middle vowel and replace it with a *
+        for (let pos = 0; pos < middle; pos++) {
+            if (middle + pos < bannedWord.length && /[aeiou]/.test(middle[middle + pos])) {
+                // Replace character with a *
+                bannedWord = bannedWord.substring(0,middle + pos) + "*" + bannedWord.substring(middle + pos+1);
+                break;
+            } else if (middle - pos < bannedWord.length && /[aeiou]/.test(middle[middle - pos])) {
+                bannedWord = bannedWord.substring(0,middle - pos) + "*" + bannedWord.substring(middle - pos+1);
+                break;
+            }
+        }
         const reply = bannedWord ? `The current banned word is: ||${bannedWord}||` : "No word is banned at the moment";
         return message.channel.send(reply);
     },
