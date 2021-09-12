5e818018360bf81f3e7b6552b592d1ff0e92d1a0
braxtonhall <braxtonjhall@gmail.com>
2021-09-11 17:55:11 -0700

    Maked censorship tests pass

--- src/commands/bans/banned.ts
+++ src/commands/bans/banned.ts
@@ -17,7 +17,7 @@ const banned: Command = {
 // Censors a word by replacing the middlemost vowel (if any) with a *. If the word is even, the middle will be
 // considered the character on the right. If there are two vowels equidistant from the middle, the left will be chosen.
 const censorWord = (word: string): string => {
-    const middle = Math.round(word.length / 2.0  - 0.5); // This makes the middle on the right size if an even length
+    const middle: number = Math.round(word.length / 2.0  - 0.5); // This makes the middle on the right size if an even length
     // Find the middle vowel and replace it with a *
     // middle + 1 gets the exact amount on the left but it could be one over on the right, we must check that
     for (let pos = 0; pos < middle + 1; pos++) {
@@ -25,10 +25,10 @@ const censorWord = (word: string): string => {
         const right = middle + pos;
         // We have to check the left first in order to reduce the bias from starting right of center on even length\
         // words. This is what allows gaga to correctly be censored as g*ga even though the middle is the second g.
-        if (/[aeiou]/.test(middle[left])) {
+        if (/[aeiou]/.test(word[left])) {
             word = word.substring(0, left) + "*" + word.substring(left + 1);
             break;
-        } else if (right < word.length && /[aeiouAEIOU]/.test(middle[right])) {
+        } else if (right < word.length && /[aeiouAEIOU]/.test(word[right])) {
             // Replace character with a *
             word = word.substring(0, right) + "*" + word.substring(right + 1);
             break;
