dc0ca394456891f5a8d843de9f88a11ef2320bcc
Braxton Hall <35436247+braxtonhall@users.noreply.github.com>
2021-10-08 00:42:26 -0700

    Show who requested banned word (#23)
    * adding support for saving the ban requester into the database
    
      * modifying `ban.ts#ban` to pass through the `Message#User#author#username`
        to the implementation of `ban` in use.
      * updating `BanController#ban` to accept the ban requester's username.
      * updating calls to `BanController#ban` in `BanContollerSpec`
    
    * adding a `BanController#getBanRequester` method
    
      * implemented `BanController#getBanRequester`.
      * adding a corresponding test in `BanControllerSpec`
      * not changing caching strategy for now; I assume that this
        functionality won't be as popular as getting the banned word itself.
        I'm of the opinion that if we do decide to cache this information,
        it should be the case that we cache the entire record from the DB,
        not just the word itself. @braxtonhall thoughts?
    
    * adding `whobanned` command support
    
      * adding `whobanned.ts` to contain the implementation for the
        `whobanned` command.
      * updating `README.md` to contain documentation for the new command.
    
    * moving `censorWord` into `Util.ts`
    
      * relocating associated tests from `test/commands/ban` to
        `test/util/Util.spec.ts`
    
    * Resolve complaints from #20
    
    * Lint
    
    Co-authored-by: James Yoo <jyoo980@gmail.com>

--- test/util/Util.spec.ts
+++ test/util/Util.spec.ts
@@ -1,5 +1,5 @@
 import {expect} from "chai";
-import {removeMarkdown} from "../../src/util/Util";
+import {removeMarkdown, censorWord} from "../../src/util/Util";
 
 describe("Util", function () {
     describe("removeMarkdown", function () {
@@ -40,6 +40,88 @@ describe("Util", function () {
             expect(removeMarkdown("test ||test test")).to.equal("test ||test test");
             expect(removeMarkdown("test `test~ test")).to.equal("test `test~ test");
             expect(removeMarkdown("test *test_ test")).to.equal("test *test_ test");
+        });
+    });
+
+    describe("censorWord", function () {
+        // a | *
+        it("should censor a single vowel", async function () {
+            return expect(censorWord("a")).to.eql("*");
+        });
+
+        // ag | *g
+        it("should censor a vowel with 1 consonant on the right", function () {
+            return expect(censorWord("ag")).to.eql("*g");
+        });
+
+        // agg | *gg
+        it("should censor a vowel with 2 consonants on the right", function () {
+            return expect(censorWord("agg")).to.eql("*gg");
+        });
+
+        // aggg | *ggg
+        it("should censor a vowel with 3 consonants on the right", function () {
+            return expect(censorWord("aggg")).to.eql("*ggg");
+        });
+
+        // agggg | *gggg
+        it("should censor a vowel with 4 consonants on the right", function () {
+            return expect(censorWord("agggg")).to.eql("*gggg");
+        });
+
+        // gga | gg*
+        it("should censor a vowel with 2 consonants on the left", function () {
+            return expect(censorWord("gga")).to.eql("gg*");
+        });
+
+        // ggga | ggg*
+        it("should censor a vowel with 3 consonants on the left", function () {
+            return expect(censorWord("ggga")).to.eql("ggg*");
+        });
+
+        // gaag | ga*g
+        it("should censor the right vowel when there are two in the middle of an even length word", function () {
+            return expect(censorWord("gaag")).to.eql("ga*g");
+        });
+
+        // gaga | g*ga
+        it("should censor the closest vowel", function () {
+            return expect(censorWord("gaga")).to.eql("g*ga");
+        });
+
+        // gggga | gggg*
+        it("should censor a vowel with 4 consonants on the left", function () {
+            return expect(censorWord("gggga")).to.eql("gggg*");
+        });
+
+        // gaagg | ga*gg
+        it("should censor the middle vowel in an odd length word", function () {
+            return expect(censorWord("gaagg")).to.eql("ga*gg");
+        });
+
+        // ggaag | gg*ag
+        it("should censor the middle vowel in an odd length word", function () {
+            return expect(censorWord("ggaag")).to.eql("gg*ag");
+        });
+
+        // gagag | g*gag
+        it("should censor the left vowel when two are the same distance", function () {
+            return expect(censorWord("gagag")).to.eql("g*gag");
+        });
+
+        // gaggag | gagg*g
+        it("should censor the right vowel when two are the same distance in an even length word", function () {
+            return expect(censorWord("gaggag")).to.eql("gagg*g");
+        });
+
+        // ggggg | ggggg
+        it("should not censor anything when there are no vowels", function () {
+            return expect(censorWord("ggggg")).to.eql("ggggg");
+        });
+
+        // A | *
+        it("should censor capital vowels", function () {
+            return expect(censorWord("A")).to.eql("*");
         });
     });
 });
