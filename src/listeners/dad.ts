6622844a1a2bd241e5597ad6e9dd59905f3d71e7
Zayd <zayd@qumsieh.net>
2021-10-08 08:36:33 -0700

    dad bot misery (#21)
    * dad bot misery
    
    * fixed regex to work this time
    
    * updated readme

---
+++ src/listeners/dad.ts
@@ -0,0 +1,25 @@
+import {Listener} from "@ubccpsc310/bot-base";
+import {Client, Message} from "discord.js";
+
+const dad: Listener<"messageCreate"> = {
+    event: "messageCreate",
+    procedure: async (client: Client, message: Message) => {
+        if (message.author.bot) return;
+
+        const messageSendersActualName = getMessageSendersActualName(message.cleanContent);
+        if (messageSendersActualName == null) return; // u have no name
+
+        const messageToSend = "Hi " + messageSendersActualName + ", I'm dad!";
+        return message.channel.send(messageToSend);
+    }
+};
+
+const getMessageSendersActualName = (content: string): string => {
+    const lowerCaseContent = content.toLowerCase();
+    const regex = /(?:^|\s)\s*(im|i'm|i am)\s+(.*)/;
+    const match = lowerCaseContent.match(regex);
+  
+    return match?.at(2);
+};
+
+export default dad;
