e51d9eaa34fa9ad8036a0b51363ff79e902bfe64
markusde <markusde@student.ubc.ca>
2021-10-09 23:52:20 -0700

    cleanup makefunny

--- src/commands/makefunny/makefunny.ts
+++ src/commands/makefunny/makefunny.ts
@@ -1,4 +1,4 @@
-import { Command, Log } from "@ubccpsc310/bot-base";
+import { Command } from "@ubccpsc310/bot-base";
 import { Client, Message } from "discord.js";
 
 // Roses are red
@@ -9,60 +9,85 @@ import * as imagizer from "imagizer";
 import request from "request";
 import * as fs from 'fs';
 
-const bufferpath = "./data/buffer.png";
+
+
+// Paramaters
+const bufferdir = "./data/";
+const buffername = "buffer.png";
+
+const ok_types = ["image/png"];
+
+enum MessageError {
+    ER_EMPTY,
+    ER_FILETYPE,
+    ER_DOWNLOAD,
+    ER_SAVE
+}
+
+const cringe: { [key in MessageError]: string } = {
+    [MessageError.ER_EMPTY]: "ahhhh omg.... jeez I wish you attached an image to me... ah aha just kidding..,.. unless? :pleading_face: :point_right: :point_left:",
+    [MessageError.ER_FILETYPE]: "uh oh I think you did a fricky wicky...  :eyes: pwease onwy call me with a png uwu",
+    [MessageError.ER_DOWNLOAD]: "oh noes my netuwurk had an errorawr", // I'm so sorry but I have to commit to the bit
+    [MessageError.ER_SAVE]: "hahah omg I'm so fricken clumsy n i lost your image I'm so sowwy"
+};
+
+
+
+function applyFunny(path, width, height) {
+    const project = new imagizer.Project(width, height);
+    const layer1 = project.createLayer();
+    const im = new imagizer.Image();
+    im.load(bufferdir + buffername, function () {
+        const obj = layer1.put(im, 0, 0);
+        obj.applyEffect("contrast", { contrast: Math.random() * 0.50 + 0.2 });
+        obj.applyEffect("brightness", { brightness: Math.random() * 0.15 + 0.05 });
+        obj.applyEffect("dither");
+        obj.applyEffect("exposure", { exposure: Math.random() * 0.3 + 0.5 });
+        obj.applyEffect("brightness", { brightness: Math.random() * 0.1 + 0.35 });
+        obj.applyEffect("contrast", { contrast: Math.random() * 0.1 + 0.7 });
+        obj.applyEffect("water", { wavelength: 30, amplitude: 0.2 * Math.random(), radius: Math.max(width, height) });
+        obj.applyEffect("sphere", { a: 0.9, b: 0.8, refractionIndex: 5.6 });
+    });
+
+    project.save(path);
+}
 
 const makefunny: Command = {
     name: "makefunny",
     description: "Makes an image funny",
     usage: "makefunny on a message with a png attached",
+
     procedure: async (client: Client, message: Message) => {
-        if (message.attachments.size === 1) {
-            await message.channel.sendTyping();
-            const img = message.attachments.entries().next().value[1];
-            const imgurl = img.attachment;
-            const imght = img.height;
-            const imgwt = img.width;
 
-            const ok_types = ["image/png"];
-
-            if (!ok_types.includes(img.contentType)) {
-                return message.channel.send("uh oh I think you did a fricky wicky...  :eyes: pwease onwy call me with a png uwu");
-            }
-
-            request.get(imgurl)
-                .on('error', Log.error)
-                .pipe(fs.createWriteStream(bufferpath))
-                .on('finish', () => {
-                    const project = new imagizer.Project(imgwt, imght);
-
-                    const layer1 = project.createLayer({
-                        blendingMode: "normal", // optional
-                    });
-
-                    const image1 = new imagizer.Image();
-                    image1.load(bufferpath, function () {
-                        const obj = layer1.put(image1, 0, 0);
-                        obj.applyEffect("contrast", { contrast: Math.random() * 0.50 + 0.2 });
-                        obj.applyEffect("brightness", { brightness: Math.random() * 0.15 + 0.05 });
-                        obj.applyEffect("dither");
-                        obj.applyEffect("exposure", { exposure: Math.random() * 0.3 + 0.5 });
-                        obj.applyEffect("brightness", { brightness: Math.random() * 0.1 + 0.35 });
-                        obj.applyEffect("contrast", { contrast: Math.random() * 0.1 + 0.7 });
-                        obj.applyEffect("water", { wavelength: 30, amplitude: 0.2 * Math.random(), radius: Math.max(imght, imgwt) });
-                        obj.applyEffect("sphere", { a: 0.9, b: 0.8, refractionIndex: 5.6 });
-                        // obj.applyEffect("gain");// , { gammaRed: 1, gammaGreen: 0.7, gammaBlue: 0.3 });
-                    });
-
-                    project.save(bufferpath);
-
-                    return message.channel.send({ files: [bufferpath] });
-
-                });
+        if (message.attachments.size !== 1) {
+            return message.channel.send(cringe[MessageError.ER_EMPTY]);
         }
-        else {
-            return message.channel.send("ahhhh omg.... jeez I wish you attached an image to me... ah aha just kidding..,.. unless? :pleading_face: :point_right: :point_left:");
+
+        await message.channel.sendTyping();
+        const img = message.attachments.entries().next().value[1];
+
+        if (!ok_types.includes(img.contentType)) {
+            return message.channel.send(cringe[MessageError.ER_FILETYPE]);
         }
-    },
+
+        if (!fs.existsSync(bufferdir)) {
+            fs.mkdirSync(bufferdir);
+        }
+
+        request.get(img.attachment)
+            .on('error', () => {
+                return message.channel.send(cringe[MessageError.ER_DOWNLOAD]);
+            })
+            .pipe(fs.createWriteStream(bufferdir + buffername))
+            .on('error', () => {
+                return message.channel.send(cringe[MessageError.ER_SAVE]);
+            })
+            .on('finish', () => {
+                applyFunny(bufferdir + buffername, img.width, img.height);
+                return message.channel.send({ files: [bufferdir + buffername] });
+            });
+
+    }
 };
 
 export default makefunny;
