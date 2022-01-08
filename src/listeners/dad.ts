03f87000c6351fb9480066582d06b10c2f1506ee
Braxton Hall <35436247+braxtonhall@users.noreply.github.com>
2022-01-07 20:35:00 -0800

    Dad come home (#39)

--- src/listeners/dad.ts
+++ src/listeners/dad.ts
@@ -5,12 +5,13 @@ const dad: Listener<"messageCreate"> = {
     event: "messageCreate",
     procedure: async (client: Client, message: Message) => {
         if (message.author.bot) return;
+        if (Math.random() > 0.01) return; // only a 1/100 chance of actually happening
 
         const messageSendersActualName = getMessageSendersActualName(message.cleanContent);
         if (messageSendersActualName == null) return; // u have no name
 
         const messageToSend = "Hi " + messageSendersActualName + ", I'm dad!";
-        // return message.channel.send(messageToSend); disabling bot for now!
+        return message.channel.send(messageToSend);
     }
 };
 
@@ -18,7 +19,7 @@ const getMessageSendersActualName = (content: string): string => {
     const lowerCaseContent = content.toLowerCase();
     const regex = /(?:^|\s)\s*(im|i'm|i am)\s+(.*)/;
     const match = lowerCaseContent.match(regex);
-  
+
     return match?.at(2);
 };
 
